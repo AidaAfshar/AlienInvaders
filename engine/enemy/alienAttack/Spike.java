10c732938ade8a3613274553a326471cfb7f6aaa
aiiiida <afsharaidam@gmail.com>
2019-05-23 12:51:48 +0430

    no important changes

--- engine/enemy/alienAttack/Spike.java
+++ engine/enemy/alienAttack/Spike.java
@@ -75,15 +75,8 @@ public class Spike extends Image {
         return width;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
     public int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
 }
