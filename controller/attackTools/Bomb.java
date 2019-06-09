19dbc39306daf3572379ee837be407ff84d83631
aiiiida <afsharaidam@gmail.com>
2019-06-10 01:09:25 +0430

    part 2 final commit!

--- controller/attackTools/Bomb.java
+++ controller/attackTools/Bomb.java
@@ -94,3 +94,84 @@ public class Bomb extends Beam {
 
 
 }
+//
+//    public static int count = 0 ;
+//    final int x0 ;
+//    final int y0 ;
+//    int xVector ;
+//    int yVector ;
+//    public boolean reachedXCenter = false ;
+//    public boolean reachedYCenter = false ;
+//    public boolean explode = false ;
+//
+//
+//
+//    BombExplosion explosion = new BombExplosion();
+//    public int j ;
+//    public boolean timeToKill = false;
+//
+//
+//    public Bomb(int x0 , int y0) {
+//        super();
+//        this.x0 = x0 ;
+//        this.y0 = y0 ;
+//        initialize();
+//    }
+//
+//    public void initialize() {
+//        this.setImage(Assets.diamondImage);
+//        this.width = 150 ;
+//        this.height = 150 ;
+//        this.setDimensions();
+//        this.xVector = getVector(Dim.CENTER_X-x0) ;
+//        this.yVector = getVector(Dim.CENTER_Y-y0) ;
+//        this.throwPermission = true ;
+////        prepareExplosionImage();
+//    }
+//
+//
+//    public static int getVector(int x) {
+//        if(x>=0) return 1 ;
+//        else
+//            return -1 ;
+//    }
+//
+//
+//    public void moveBomb() {
+//        System.out.println("inside moveBomb 1");
+//        if(Math.abs(x-Dim.CENTER_X)>4) {
+//            x+= xVector*3 ;
+//        }else {
+//            reachedXCenter=true ;
+//        }
+//        if(Math.abs(y-Dim.CENTER_Y)>4) {
+//            y+= yVector*3 ;
+//        }else {
+//            reachedYCenter=true ;
+//        }
+//        if(reachedXCenter && reachedYCenter) {
+//            System.out.println("inside moveBomb 2");
+//            this.explode = true ;
+//            this.timeToKill = true ;
+//            this.throwPermission = false ;
+//        }
+//    }
+//
+////    public void prepareExplosionImage() {
+////        explosionImage = new Image();
+////        explosionImage.setAddress(explosionImageAddress) ;
+////        explosionImage.setImage(ImageLoader.Load(explosionImage.getAddress()));
+////    }
+//
+//
+//    public void renderExplosion(Graphics g) {
+//        g.drawImage(explosion.explosionImages[j].getImage(),Dim.CENTER_X-120,Dim.CENTER_Y-120,200,200,null);
+//
+//    }
+//
+//
+//    @Override
+//    public void draw(Graphics g){
+//        super.draw(g);
+//        System.out.println("inside draw");
+//    }
